10b114b50f2b5da0265ee8e288d34ee42c108a25
saljafa0 <169013113+saljafa0@users.noreply.github.com>
2024-05-05 23:31:42 -0400

    Add files via upload

---
+++ Project2-javascripts.js
@@ -0,0 +1,27 @@
+function renderTime(){
+		var myDate= new Date();
+		var year = .get year();
+		var day = mydate.getDay();
+		var month = mydate.getMonth();
+	}
+
+const submit = document.Form("submit")
+
+button.addEventListener("click", () => {
+	Notification.requestPermission().then(perm => {
+		if (perm ==="granted"){
+			new
+		alert(perm)
+	})
+})
+runClock();
+		setInterval("runClock()", 1000);
+		function runClock(){
+			var currentDate= new Date();
+			var datStr = currentDay.toLocalDateString();
+			var timeStr = currentDay.toLacalTimeString();
+		
+		document.getElementByID("dateNow").innerHTML = dateStr + "<br /> + timeStr;
+		</script>
+		<script>alert("create your account!!");</script>
+console
